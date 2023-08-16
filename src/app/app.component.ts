e7b5663054e0333af6609caad5ebf2f9834fb593
ardiekevin <140492883+ardiekevin@users.noreply.github.com>
2023-08-16 19:25:33 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: 'ta9-dropdown-button',
+  templateUrl: './dropdown-button.component.html',
+  styleUrls: ['./dropdown-button.component.scss'],
 })
-export class AppComponent {
-  title = 'Angular';
+
+
+export class DropdownButtonComponent {
+  isDropdownOpen = false;
+  selectedOption = 'Last 30 Minutes'; // Initialize the property
+
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  selectOption(option: string) {
+    this.selectedOption = option;
+    this.isDropdownOpen = false; // Close the dropdown after selection
+  }
 }
-
-
-
-
-
